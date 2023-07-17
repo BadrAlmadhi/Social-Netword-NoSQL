f9345f7bbc26faab9fa451b55dfeb7903d2055b5
Badr Almadi <badr.almadhi95@gmail.com>
2023-07-16 19:31:22 -0600

    found a problem

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const thoughtRoutes = require('./thoughtRoute');
 const userRoutes = require('./userRoute');
 
-router.use('/uer', userRoutes);
+router.use('/user', userRoutes);
 router.use('/thought', thoughtRoutes);
 
 
